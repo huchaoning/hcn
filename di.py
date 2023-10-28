2e78c91249cebbc1404d5c477582819c559c9c3e
vxyi <vxyi@qq.com>
2023-10-29 04:03:24 +0800

    测试后得知, 先把图片按照一个方向求和的速度要比先矩阵乘法快得多, 换成了先求和

--- di.py
+++ di.py
@@ -2,11 +2,19 @@ import numpy as np
 
 zero_point = 0
 pixel_size = 1
+axis = 'y'
 
-def estimator(img):
-    index = np.arange(np.shape(img)[0])
-    normalized_img = img / img.sum()
-    return ((index @ normalized_img).sum() - zero_point) * pixel_size
+def estimator(img, axis = axis):
 
-if __name__ == "__main__":
-    main()
+    if axis == 'x':
+        img = np.sum(img, axis=0)
+        img = img / img.sum()
+        index = np.arange(len(img))
+        return (index @ img - zero_point) * pixel_size
+    
+    elif axis == 'y':
+        img = np.sum(img, axis=1)
+        img = img / img.sum()
+        index = np.arange(len(img))
+        return (index @ img - zero_point) * pixel_size
+
