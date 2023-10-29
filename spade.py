e2eef0a857ad337045234f083d158c23a7c62069
vxyi <vxyi@qq.com>
2023-10-29 21:26:17 +0800

    添加了使用 00 01 模式做模式分解的估计子

--- spade.py
+++ spade.py
@@ -4,7 +4,11 @@ point_1 = None
 point_2 = None
 characteristic_width = 1
 
-def estimator(img):
-    k = sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
-    return 2 * characteristic_width * (k-1)/(k+1)
+def estimator(img, mode = '+-'):
+    if mode == '+-':
+        k = sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
+        return 2 * characteristic_width * (k-1)/(k+1)
+    elif mode == '0001':
+        return 2 * characteristic_width * sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
+
 
