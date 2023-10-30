db20a369c59f1eaaa675028528b4943e716bec54
vxyi <vxyi@qq.com>
2023-10-30 14:58:50 +0800

    添加了输出光子数信息的函数. 估计子添加了判断条件以应对N=0的情况

--- spade.py
+++ spade.py
@@ -1,15 +1,12 @@
 from math import sqrt
 
-
 point_1 = None
 point_2 = None
 characteristic_width = 1
 mode = '+-'
 
-
 def photon_number(img):
-    return (img[point_1[1]][point_1[0]], img[point_2[1]][point_2[0]])
-
+    return img[point_1[1]][point_1[0]], img[point_2[1]][point_2[0]]
 
 def estimator(img, mode = mode):
 
@@ -21,5 +18,4 @@ def estimator(img, mode = mode):
             return 2 * characteristic_width * (1-k)/(1+k)
         elif mode == '0001':
             return 2 * characteristic_width * sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
-
 
