17461095b40c30cb1815414ee8cbf9337ef2feea
vxyi <vxyi@qq.com>
2023-10-30 13:30:06 +0800

    添加了对 N = 0 的情况的处理, 添加了输出光子数 (灰度值) 信息的函数

--- di.py
+++ di.py
@@ -1,8 +1,14 @@
 import numpy as np
 
+
 zero_point = 0
 pixel_size = 1
 axis = 'y'
+
+
+def photon_number(img):
+    return img.sum()
+
 
 def estimator(img, axis = axis):
 
@@ -10,11 +16,11 @@ def estimator(img, axis = axis):
         img = np.sum(img, axis=0)
         img = img / img.sum()
         index = np.arange(len(img))
-        return (index @ img - zero_point) * pixel_size
+        return (zero_point - index @ img) * pixel_size
     
     elif axis == 'y':
         img = np.sum(img, axis=1)
         img = img / img.sum()
         index = np.arange(len(img))
-        return (index @ img - zero_point) * pixel_size
+        return (zero_point - index @ img) * pixel_size
 
