cbbab6a7934a17268a8cf68454b04beefb6fa818
vxyi <vxyi@qq.com>
2023-11-03 20:46:39 +0800

    添加了相机的 CF, QE, Offset 值和一个函数用于计算光子数的函数

--- qcmos.py
+++ qcmos.py
@@ -1,1 +1,9 @@
 pixel_size = 4.6
+conversion_factor = 0.107
+
+quantum_efficiency = 0.56
+offset = 200
+
+def grayscale_value_to_photon_number(grayscale_value):
+    return (grayscale_value - offset) * conversion_factor / quantum_efficiency
+
