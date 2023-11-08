838d1aef1f20288e1068739b4733a4688b4edfab
vxyi <vxyi@qq.com>
2023-11-08 22:29:35 +0800

    将CGH单独作为了一个模块

--- equipments/slm.py
+++ equipments/slm.py
@@ -1,29 +1,3 @@
-import numpy as np
-
+pixel_size = 8
 resolution = (1920, 1080)
-
-arrizon1 = 1
-arrizon2 = 2
-
-class cgh:
-    def __init__(self, method):
-        self.method = method
-
-    def fx(self, original=False):
-        from os import path
-        fx = np.load(path.join(path.dirname(__file__), f'fx{self.method}.npy'))
-        del path
-        if original:
-            return fx
-        else:
-            from scipy.interpolate import interp1d
-            fx = interp1d(np.linspace(0, 1, 801), fx)
-            del interp1d
-            return fx
-        
-    def gen(self, beam):
-        pass
-
-    
-    
 
