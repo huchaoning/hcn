2ebedea9a07aef0af065c8adcec21e6721ae99ee
vxyi <vxyi@qq.com>
2023-11-09 14:37:54 +0800

    DI的估计子和光子数模块

--- di.py
+++ di.py
@@ -1,23 +1,18 @@
 import numpy as np
 
-
 zero_point = 0
 pixel_size = 1
 axis = 'y'
 
-
 def photon_number(img):
     return img.sum()
 
-
 def estimator(img, axis = axis):
-
     if axis == 'x':
         img = np.sum(img, axis=0)
         img = img / img.sum()
         index = np.arange(len(img))
         return (zero_point - index @ img) * pixel_size
-    
     elif axis == 'y':
         img = np.sum(img, axis=1)
         img = img / img.sum()
