ebafbdd1a217d4023d5579aa89cd2c0e79eb4f1e
vxyi <vxyi@qq.com>
2023-11-14 15:38:42 +0800

    更正了CGH的代码, 叠加正弦相位光栅是在相位项上面的

--- cgh.py
+++ cgh.py
@@ -18,19 +18,20 @@ def fx(method=2, original=False):
 def gen(complex_amplitude=None, method=2, nx=500, ny=0): 
     v, h = np.shape(complex_amplitude)
     x, y = np.meshgrid(np.arange(-h/2, h/2), -np.arange(-v/2, v/2))
+
     f = fx(method=method)
 
     a = np.abs(complex_amplitude) / np.abs(complex_amplitude).max()
-    phi = np.angle(complex_amplitude) - (np.pi/2)
+    phi = np.angle(complex_amplitude)
 
     if method == 1:
-        img = phi + f(a) * np.sin(phi)
+        img = phi + f(a) * np.sin(phi + (2*np.pi*(x*nx/h+y*ny/v)))
     elif method == 2:
-        img = f(a) * np.sin(phi)
+        img = f(a) * np.sin(phi + (2*np.pi*(x*nx/h+y*ny/v)))
 
     from .__init__ import max_min_normalization as nl
-    img = nl(img * np.sin(2*np.pi*(x*nx/h+y*ny/v))) * 255
+    img = nl(img) * 255
     del nl
 
-    return img.astype(np.uint8)
+    return img
 
