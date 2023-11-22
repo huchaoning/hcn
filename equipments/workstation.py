f45c8a856445f76cb8ffca0e8c2d5566329df356
vxyi <vxyi@qq.com>
2023-11-23 00:13:15 +0800

    稍微修改了一下代码格式

--- equipments/workstation.py
+++ equipments/workstation.py
@@ -2,5 +2,4 @@ def ip():
     from urllib import request
     ip = request.urlopen('https://gitee.com/vxyi/quantum-ws-ip-address/raw/master/ip')
     print(ip.read().decode('utf-8'))
-    del request
 
