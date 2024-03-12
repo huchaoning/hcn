7a832fe6e4e1d12cc7340ff3f0efa88dcf0fc58e
vxyi <vxyi@qq.com>
2024-03-12 16:11:50 +0800

    增加了batch估计子, 把默认参数设置为了实验上的测值

--- experiments/di.py
+++ experiments/di.py
@@ -1,27 +1,119 @@
 import numpy as np
 
-zero_point = 0
-pixel_size = 1
+zero_point = 111
+pixel_size = 4.6
+ax_1d = 81
+
+use_1d_only = True
+batch = True
 
 def photon_number(img):
+    if batch and (not use_1d_only):
+        raise('if batch is True, use_1d_only must be True')
+    if batch:
+        img = img[:, :, ax_1d]
+        return img.sum(axis=1)
+    elif use_1d_only:
+        img = img[:, ax_1d]
     return img.sum()
 
-def estimator(img, axis = 'y'):
-    if axis == 'x':
-        img = np.sum(img, axis=0)
-        img = img / img.sum()
-        index = np.arange(len(img))
-        return (zero_point - index @ img) * pixel_size
-    elif axis == 'y':
-        img = np.sum(img, axis=1)
-        img = img / img.sum()
-        index = np.arange(len(img))
-        return (zero_point - index @ img) * pixel_size
 
-def estimator_1d(img):
-    if len(np.shape(img)) != 1:
-        raise ValueError('img must be 1d')
+def estimator(img, axis = 'y'):
+    if batch and (not use_1d_only):
+        raise('if batch is True, use_1d_only must be True')
+    if batch:
+        img_shape = img.shape
+        img_sum = img.sum(axis=1)
+        index = np.arange(img_shape[1])
+        normalized_img = img / img_sum.reshape(img_shape[0], 1)
+        return (zero_point - normalized_img @ index)
+    if use_1d_only:
+        img = img[:, ax_1d]
+        if len(np.shape(img)) != 1:
+            raise ValueError('img must be 1d')
+        else:
+            img = img / img.sum()
+            index = np.arange(len(img))
+            return (zero_point - index @ img) * pixel_size
     else:
-        img = img / img.sum()
-        index = np.arange(len(img))
-        return (zero_point - index @ img) * pixel_size
+        if axis == 'x':
+            img = np.sum(img, axis=0)
+            img = img / img.sum()
+            index = np.arange(len(img))
+            return (zero_point - index @ img) * pixel_size
+        elif axis == 'y':
+            img = np.sum(img, axis=1)
+            img = img / img.sum()
+            index = np.arange(len(img))
+            return (zero_point - index @ img) * pixel_size
+
+
+
+# import numpy as np
+# from ..plotter import imread
+
+# zero_point = 0
+# pixel_size = 1
+
+# __all__ = ['photon_number', 'estimator']
+
+# def photon_number(img):
+#     return img.sum()
+
+# def estimator_single(img, axis = 'y'):
+#     if axis == 'x':
+#         img = np.sum(img, axis=0)
+#         img = img / img.sum()
+#         index = np.arange(len(img))
+#         return (zero_point - index @ img) * pixel_size
+#     elif axis == 'y':
+#         img = np.sum(img, axis=1)
+#         img = img / img.sum()
+#         index = np.arange(len(img))
+#         return (zero_point - index @ img) * pixel_size
+
+# def estimator_single_1d(img):
+#     if len(np.shape(img)) != 1:
+#         raise ValueError('img must be 1d')
+#     else:
+#         img = img / img.sum()
+#         index = np.arange(len(img))
+#         return (zero_point - index @ img) * pixel_size
+
+
+# def estimator(data, axis='y', batch=True):
+#     if type(data) is str:
+#         data = imread(data)
+#     elif type(data) is not np.ndarray:
+#         raise ValueError('data is invalid, data must be image path or a numpy array')
+#     elif data.ndim > 3:
+#         raise ValueError('data is invalid, data must be a single tif or multi-tifs')
+
+#     if batch and (data.ndim == 2):
+#         _1d = True
+#     elif (not batch) and (data.ndim == 1):
+#         _1d = True
+#     else:
+#         _1d = False
+
+#     if batch:
+#         if _1d and len(data.shape) != 2:
+#             samples, y = data.shape
+#             index = np.arange(y)
+#         elif not _1d:
+#             samples, y, x = data.shape
+#             if axis == 'x':
+#                 data = data.sum(axis=1)
+#                 index = np.arange(x)
+#             if axis == 'y':
+#                 data = data.sum(axis=2)
+#                 index = np.arange(y)
+#         data = data / data.sum(axis=1).reshape(samples, 1)
+#         return (zero_point - data @ index) * pixel_size
+    
+#     elif _1d:
+#         return estimator_single_1d(data)
+    
+#     elif not _1d:
+#         return estimator_single(data)
+
--- experiments/spade.py
+++ experiments/spade.py
@@ -1,22 +1,35 @@
 from math import sqrt
+import numpy as np
 
-point_1 = None
-point_2 = None
-characteristic_width = 1
+point_1 = (91, 110)
+point_2 = (91, 404)
+characteristic_width = 103
 
 mode = '+-'
+batch = True
 
 def photon_number(img):
-    return img[point_1[1]][point_1[0]], img[point_2[1]][point_2[0]]
-
+    if batch and (mode != '+-'):
+        raise('if batch is True, mode must be +-')
+    if batch:
+        return img[:, point_1[1], point_1[0]] + img[:, point_2[1], point_2[0]]
+    else:
+        return img[point_1[1], point_1[0]] + img[point_2[1], point_2[0]]
 
 def estimator(img, mode = mode):
-    if img[point_2[1]][point_2[0]] == 0:
-        return - 2 * characteristic_width
+    if batch and (mode != '+-'):
+        raise('if batch is True, mode must be +-')
+    if batch:
+        k = np.sqrt(img[:, point_1[1], point_1[0]] / img[:, point_2[1], point_2[0]])
+        result = 2 * characteristic_width * (1-k)/(1+k)
+        result[np.logical_not(np.isfinite(k))] = -2 * characteristic_width
+        return result
     else:
-        if mode == '+-':
-            k = sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
+        if img[point_2[1], point_2[0]] == 0:
+            return -2 * characteristic_width
+        elif mode == '+-':
+            k = sqrt(img[point_1[1], point_1[0]] / img[point_2[1], point_2[0]])
             return 2 * characteristic_width * (1-k)/(1+k)
         elif mode == '0001':
-            return 2 * characteristic_width * sqrt(img[point_1[1]][point_1[0]] / img[point_2[1]][point_2[0]])
+            return 2 * characteristic_width * sqrt(img[point_1[1], point_1[0]] / img[point_2[1], point_2[0]])
 
