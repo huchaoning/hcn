a11d8d84d1fbdcff32471646cd1472c7243ed667
vxyi <vxyi@qq.com>
2024-03-21 18:09:28 +0800

    通过对说明书的 QE 曲线进行识别并插值, 给出一个插值后的函数用于输出某个波长下的 QE

--- equipments/qcmos.py
+++ equipments/qcmos.py
@@ -1,7 +1,11 @@
-pixel_size = 4.6
+import numpy as np
+from scipy.interpolate import interp1d
+from os import path
 
+pixel_size = 4.6
 conversion_factor = 0.107
-quantum_efficiency = 0.56
+quantum_efficiency = interp1d(np.linspace(250, 1100, 8501), 
+                              np.load(file=path.join(path.dirname(__file__), 'qe.npy')))
 
 def grayscale_value_to_photon_number(grayscale_value, offset=None):
     return (grayscale_value - offset) * conversion_factor / quantum_efficiency
