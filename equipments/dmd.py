332aadefcc263c0a4194c14282eeae939ff31465
vxyi <vxyi@qq.com>
2024-09-02 16:15:52 +0800

    DMD

--- equipments/dmd.py
+++ equipments/dmd.py
@@ -2,6 +2,8 @@ import numpy as np
 
 pixel_size = 19.374725804511403
 
-def picture_time(f):
+def picture_time(f, round=True, expo_time=45100):
+    if round:
+        t = (picture_time(f, False) // expo_time) * expo_time
+        return t, 1e6/(2*t)
     return np.round(1000000 / (2*f)).astype(int)
-
