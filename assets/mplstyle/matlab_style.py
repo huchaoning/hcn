576ebf502d08d19e54b9f715faf07fdc523b50f6
vxyi <vxyi@qq.com>
2025-09-18 02:51:33 +0800

    以样式的方式临时使用

---
+++ assets/mplstyle/matlab_style.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib_inline import backend_inline
+backend_inline.set_matplotlib_formats('retina')
+del backend_inline
+
+import os
+from matplotlib.font_manager import fontManager
+assets_dir = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')
+)
+fontManager.addfont(os.path.join(assets_dir, 'fonts/SourceHanSans.otf'))
+del fontManager
+
+
+plt.rcParams['font.size'] = 7
+plt.rcParams['font.family'].append('Source Han Sans SC')
+
+plt.rcParams['axes.linewidth'] = 0.5
+plt.rcParams['xtick.major.width'] = 0.5
+plt.rcParams['ytick.major.width'] = 0.5
+plt.rcParams['figure.dpi'] = 150
+plt.rcParams['figure.figsize'] = (3.3, 2.5)
+plt.rcParams['lines.linewidth'] = 1.5
+
+plt.rcParams['xtick.top'] = True
+plt.rcParams['ytick.right'] = True
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['xtick.major.size'] = 2
+plt.rcParams['ytick.major.size'] = 2
+
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.rcParams['axes.prop_cycle'] = \
+    plt.cycler('color', ['#0072bd', '#d95319', '#edb120', '#7e2f8e', '#77ac30', '#4dbeee', '#a2142f']) + \
+    plt.cycler('linestyle', ['-', '--', '-.', ':', '-', '--', '-.'])
+
+plt.rcParams['legend.fancybox'] = False
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.framealpha'] = 1.0
+plt.rcParams['legend.edgecolor'] = 'k'
+plt.rcParams['patch.linewidth'] = 0.5
+
+plt.rcParams['axes.unicode_minus'] = False
+
+
+import matplotlib as mpl
+
+parula = mpl.colors.LinearSegmentedColormap.from_list('parula', np.load(os.path.join(assets_dir, 'mplstyle/parula_map.npy')))
+mpl.colormaps.register(parula)
+plt.rcParams['image.cmap'] = 'parula'
+del mpl, parula
+
+
+__all__ = []
